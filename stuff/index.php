1f6cc2468d44c5362fa711ddd51c3b5383ff2e8d
guelfoweb <guelfoweb@gmail.com>
2012-12-08 19:13:28 +0100

    first commit

---
+++ stuff/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>WP_WAF</title>
+</head>
+<body>
+	<center>
+	<img src="wp_waf.png" />
+	<br>
+	See <a href="README">README</a> file for more info.
+	</center>
+</body>
+</html>
