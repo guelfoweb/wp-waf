f83e3770cdc594568cc0bd7d3d06d1a5a7d084e8
Gianni Amato <guelfoweb@gmail.com>
2013-05-26 21:25:50 +0300

    Update uninstall.php
    v.2.0 restore original.htaccess

--- uninstall.php
+++ uninstall.php
@@ -5,10 +5,28 @@
  * @since 1.0
  */
 
-
 // Check for the 'WP_UNINSTALL_PLUGIN' constant, before executing
 if( !defined( 'ABSPATH' ) && !defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit();
 
+
+$htaccess = '../.htaccess';
+$htaback  = '../original.htaccess';
+
+if (file_exists($htaccess)) {
+	if (file_exists($htaback)) {
+		/* remove original .htaccess */
+		unlink($htaccess);
+		/* replace .htaccess with original.htaccess */
+		copy($htaback, $htaccess);	
+		unlink($htaback);
+	}
+}
+
+
+$args = array(
+	'waf_settings'
+);
+
 // Delete options from the database
-delete_option( 'waf_settings' );
+delete_option( $args );
